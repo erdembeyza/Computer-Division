9b17f12d00fa63b7f20e00ded3d93899d60e8f0b
Beyza <erdembeyza.be@gmail.com>
2026-03-21 23:39:19 +0300

    basic operation

--- basicoperation.py
+++ basicoperation.py
@@ -1,4 +1,4 @@
-import os
+"""import os
 import cv2 as cv
 from numpy.ma.core import resize
 
@@ -13,4 +13,22 @@ print(resize_images.shape)
 cv.imshow("cat", image)
 cv.imshow("resize", resize_images)
 cv.waitKey(0)
-#ekranda bastırıldı
+#ekranda bastırıldı"""
+
+"""------------"""
+#crop
+import matplotlib.pyplot as plt
+import os
+import cv2 as cv
+img= cv.imread(os.path.join(".", "cat.jpg"))
+print(img.shape)
+crop_img= img[120:640,420:840]
+#yukarıdan 120 pikselden 640. piksele kadar. soldan 420.den 840.piksele kadar
+cv.imshow("img", img)
+cv.imshow("crop_img", crop_img)
+cv.waitKey(0)
+
+#koordinatları görebilmek için: matplot kütüphanesi
+img_rgb= cv.cvtColor(img, cv.COLOR_BGR2RGB)
+plt.imshow(img_rgb)
+plt.show()
